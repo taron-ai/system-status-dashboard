c4a05f4a3991f877a8ee8a750c0c548a9a67c170
tom.alessi <tom.alessi@e7548c54-4461-a05d-3534-fe9a90f6f4c6>
2012-11-09 16:02:46 +0000

    Adding generic local settings directory

---
+++ local.tmp/local_settings.py
@@ -0,0 +1,41 @@
+"""SSD Local Configuration File
+
+   Required Configuration
+    - Database username
+    - Database password
+      (note: database host/port can be ignored if its local and using the standard port)
+    - Secret key
+    - Full path to template directory
+
+   Optional configuration
+    - Everything else
+   
+"""
+
+### Required ###
+
+DATABASES = {
+    'default': {
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : 'ssd',
+        'USER'     : '<<ENTER DATABASE USER NAME>>',       
+        'PASSWORD' : '<<ENTER DATABASE USER PASSWORD>>', 
+        'HOST'     : '', # Leave blank if the database is on the local host
+        'PORT'     : '', # Leave blank if the default mysql port is being used        
+    }
+}
+SECRET_KEY = '<<ENTER SECRET KEY>>'
+TEMPLATE_DIRS = (
+    '<<ENTER FULL PATH>>'
+)
+
+
+
+### Optional ###
+
+#NAV = True
+#REPORT_INCIDENT = True
+#CONTACTS = True
+#NOTIFY = True
+#TIME_ZONE = 'America/Chicago'
+#LOGO = ''

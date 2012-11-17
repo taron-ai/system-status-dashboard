8470dee6cdf6149c56e3dcb3998e44f8273f964b
tom.alessi <tom.alessi@e7548c54-4461-a05d-3534-fe9a90f6f4c6>
2012-11-17 14:50:28 +0000

    Tag Release 1.0

---
+++ tags/REL-1.0/local.tmp/local_settings.py
@@ -0,0 +1,45 @@
+"""SSD Local Configuration File
+
+   Required Configuration
+    - Database username
+    - Database password
+      (note: database host/port can be ignored if its local and using the standard port)
+    - Secret key
+    - Full path to template directory
+
+   Optional configuration
+    - Everything else
+   
+"""
+
+### Required ###
+
+DATABASES = {
+    'default': {
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : 'ssd',
+        'USER'     : '<<ENTER DATABASE USER NAME>>',       
+        'PASSWORD' : '<<ENTER DATABASE USER PASSWORD>>', 
+        'HOST'     : '', # Leave blank if the database is on the local host
+        'PORT'     : '', # Leave blank if the default mysql port is being used        
+    }
+}
+SECRET_KEY = '<<ENTER SECRET KEY>>'
+TEMPLATE_DIRS = (
+    '<<ENTER FULL PATH>>/templates'
+)
+
+
+
+### Optional ###
+
+# The following are already set in the project settings.py configuration
+# file but may be overridden here
+
+#NAV = True
+#REPORT_INCIDENT = True
+#CONTACTS = True
+#NOTIFY = True
+#TIME_ZONE = ''
+#LOGO = ''
+#SSD_URL = ''

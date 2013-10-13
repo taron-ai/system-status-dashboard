32bf73e98488fffe2d5c5d13a339c3af03a430b5
tom.alessi <tom.alessi@e7548c54-4461-a05d-3534-fe9a90f6f4c6>
2013-10-13 22:11:59 +0000

    Adding memcached stats to admin

--- trunk/ssd/main/views/admin.py
+++ trunk/ssd/main/views/admin.py
@@ -18,6 +18,8 @@
 
 
 import logging
+from django.conf import settings
+from django.core.cache import cache, get_cache
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response
@@ -39,12 +41,59 @@ def main(request):
 
     logger.debug('%s view being executed.' % 'admin.main')
 
+
+    # -- MEMCACHED INFORMATION --#
+
+    m_stats = []
+
+    # Table Headings, will look like this:
+    # ['Statistic','Server1','Server2','Server3']
+    headings = ['Statistic']
+
+    # Table rows, will look like this:
+    # {
+    #   'auth_cmd':[2,3,4],
+    #   'curr_connections':[7,3,2]
+    # }
+    rows = {}
+
+    m_stats = [headings,rows]
+
+    # Note, headings and rows should have the same number of elements
+    # or something is wrong
+    if hasattr(settings, 'CACHES'):
+        try:
+            for c in settings.CACHES:
+                stats = get_cache(c)._cache.get_stats()
+  
+                for server_cache in stats:
+                    server = server_cache[0]
+                    current_stats = server_cache[1]
+
+                    # Add the server to the headings
+                    headings.append(server)
+
+                    # Iterate over the stats and add to the rows dict
+                    for key,value in current_stats.items():
+                        # If the relevant list does not exist yet, create it
+                        if not key in rows:
+                            rows[key] = []
+
+                        rows[key].append(value)
+
+        except Exception as e:
+            logger.error('Cannot obtain memcached settings: %s' % e)
+    else:
+        logger.debug('No memcached caches are defined.')
+
+
     # Print the page
     return render_to_response(
        'admin/main.html',
        {
           'title':'System Status Dashboard | Admin',
           'version':get_version,
+          'm_stats':m_stats,
           'breadcrumbs':{'Admin':'/admin'}
        },
        context_instance=RequestContext(request)
